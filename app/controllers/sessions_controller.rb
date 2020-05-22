863c3bab55fa1f85f17c6f4dfe195c748dbd711b
Aiandrox <aiandrox7@gmail.com>
2020-05-22 16:03:13 +0900

    [Fix] コントローラでSessionHelperを読み込む

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
+
   def new; end
 
   # TODO: NoMethodError: undefined method `authenticate' for nil:NilClass
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user.authenticate(params[:session][:password])
-      # TODO: NoMethodError: undefined method `log_in' for #<SessionsController:0x00007fb0e7c9c760>
       log_in user
       redirect_to user
     else
