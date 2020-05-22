94291519628fa80b5830c9a5542207d1081f5f3e
Aiandrox <aiandrox7@gmail.com>
2020-05-22 15:10:19 +0900

    [Add] テストが通らなかったところをメモ

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
   def new; end
 
+  # TODO: NoMethodError: undefined method `authenticate' for nil:NilClass
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user.authenticate(params[:session][:password])
+      # TODO: NoMethodError: undefined method `log_in' for #<SessionsController:0x00007fb0e7c9c760>
       log_in user
       redirect_to user
     else
