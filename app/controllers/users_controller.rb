f7fee32f77eed4913c7277f3211630d7bdf1ae38
sho171 <sh.zxcv00@gmail.com>
2020-09-27 15:52:44 +0900

    modify bag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  include SessionsHelper
   def new
     @user = User.new
   end
@@ -21,7 +22,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email,
+    params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
 end
